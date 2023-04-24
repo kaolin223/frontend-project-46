43874c1999fe4535468758ceda68e96c5f7f05a3
kaolin223 <m-kuptsov@inbox.ru>
2023-04-24 14:20:09 +0300

    fix code in gendiffLogic

--- src/gendiffLogic.js
+++ src/gendiffLogic.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
-
+import { readFileSync } from 'fs';
 import _ from 'lodash';
 
 const genDiff = (data1, data2) => {
-  const key1 = Object.keys(data1);
-  const key2 = Object.keys(data2);
-  const keys = _.union(key1, key2);
-  const map = keys.map((key) => {
-    if (!Object.hasOwn(data1, key)) {
+  const keys = _.union(_.keys(data1), _.keys(data2));
+  const sortedKeys = _.sortBy(keys);
+  const map = sortedKeys.map((key) => {
+    if (!_.has(data1, key)) {
       return { key, type: 'added', value: data2[key] };
     }
-    if (Object.hasOwn(data2, key)) {
+    if (!_.has(data2, key)) {
       return { key, type: 'deleted', value: data1[key] };
     }
     if (data1[key] !== data2[key]) {
@@ -23,4 +22,10 @@ const genDiff = (data1, data2) => {
   return map;
 };
 
-export default genDiff;
+export default (filepath1, filepath2) => {
+  const file1 = readFileSync(filepath1, 'utf-8');
+  const file2 = readFileSync(filepath2, 'utf-8');
+  const parseFile1 = JSON.parse(file1);
+  const parseFile2 = JSON.parse(file2);
+  console.log(genDiff(parseFile1, parseFile2));
+};
