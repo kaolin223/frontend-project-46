f3754579df4ce2baee4ca1317bd74d3274417dce
kaolin223 <m-kuptsov@inbox.ru>
2023-04-24 16:00:14 +0300

    try step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
+import { Command } from 'commander';
+import diff from '../src/gendiffLogic.js';
+
+const program = new Command();
 
 program
-  .version('0.0.1')
+  .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format')
-  .arguments('')
-  .parse();
+  .arguments('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(diff(filepath1, filepath2));
+  });
+
+program.parse(process.argv);
--- src/gendiffLogic.js
+++ src/gendiffLogic.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-import { readFileSync } from 'fs';
 import _ from 'lodash';
+import { readFileSync } from 'fs';
 
-const genDiff = (data1, data2) => {
+const gendiff = (data1, data2) => {
   const keys = _.union(_.keys(data1), _.keys(data2));
   const sortedKeys = _.sortBy(keys);
   const map = sortedKeys.map((key) => {
@@ -22,10 +22,31 @@ const genDiff = (data1, data2) => {
   return map;
 };
 
-export default (filepath1, filepath2) => {
+const stringify = (value, replacer = ' ', spacesCount = 1) => {
+  const iter = (currentValue, depth) => {
+    if (typeof currentValue !== 'object' || currentValue === null) {
+      return `${currentValue}`;
+    }
+    const indentSize = depth * spacesCount;
+    const currentIndent = replacer.repeat(indentSize);
+    const bracketIndent = replacer.repeat(indentSize - spacesCount);
+    const lines = Object.entries(currentValue).map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
+    return [
+      '{',
+      ...lines,
+      `${bracketIndent}}`,
+    ].join('\n');
+  };
+  return iter(value, 1);
+};
+
+const diff = (filepath1, filepath2) => {
   const file1 = readFileSync(filepath1, 'utf-8');
   const file2 = readFileSync(filepath2, 'utf-8');
   const parseFile1 = JSON.parse(file1);
   const parseFile2 = JSON.parse(file2);
-  console.log(genDiff(parseFile1, parseFile2));
+  console.log(gendiff(parseFile1, parseFile2));
+  console.log(stringify(gendiff(parseFile1, parseFile2)));
 };
+
+export default diff;
