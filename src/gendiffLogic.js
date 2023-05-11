1312b3d3c8b1ec0382acef696b0f22675ae205c9
kaolin223 <m-kuptsov@inbox.ru>
2023-05-11 11:03:38 +0300

    refactoring step 3

--- src/gendiffLogic.js
+++ src/gendiffLogic.js
@@ -1,52 +1,24 @@
 #!/usr/bin/env node
+
 import _ from 'lodash';
-import { readFileSync } from 'fs';
 
-const gendiff = (data1, data2) => {
-  const keys = _.union(_.keys(data1), _.keys(data2));
-  const sortedKeys = _.sortBy(keys);
-  const map = sortedKeys.map((key) => {
-    if (!_.has(data1, key)) {
-      return { key, type: 'added', value: data2[key] };
+const buildDiffTree = (first, second) => {
+  const keys = _.sortBy(_.union(_.keys(first), _.keys(second)));
+  const tree = keys.map((key) => {
+    if (!_.has(second, key)) {
+      return { key, state: 'deleted', value: first[key] };
     }
-    if (!_.has(data2, key)) {
-      return { key, type: 'deleted', value: data1[key] };
+    if (!_.has(first, key)) {
+      return { key, state: 'added', value: second[key] };
     }
-    if (data1[key] !== data2[key]) {
-      return {
-        key, type: 'changed', value1: data1[key], value2: data2[key],
-      };
+    if (_.isEqual(first[key], second[key])) {
+      return { key, state: 'unchanged', value: first[key] };
     }
-    return { key, type: 'unchanged', value: data2[key] };
+    return {
+      key, state: 'changed', value: second[key], oldValue: first[key],
+    };
   });
-  return map;
+  return tree;
 };
 
-const stringify = (value, replacer = ' ', spacesCount = 1) => {
-  const iter = (currentValue, depth) => {
-    if (typeof currentValue !== 'object' || currentValue === null) {
-      return `${currentValue}`;
-    }
-    const indentSize = depth * spacesCount;
-    const currentIndent = replacer.repeat(indentSize);
-    const bracketIndent = replacer.repeat(indentSize - spacesCount);
-    const lines = Object.entries(currentValue).map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
-    return [
-      '{',
-      ...lines,
-      `${bracketIndent}}`,
-    ].join('\n');
-  };
-  return iter(value, 1);
-};
-
-const diff = (filepath1, filepath2) => {
-  const file1 = readFileSync(filepath1, 'utf-8');
-  const file2 = readFileSync(filepath2, 'utf-8');
-  const parseFile1 = JSON.parse(file1);
-  const parseFile2 = JSON.parse(file2);
-  console.log(gendiff(parseFile1, parseFile2));
-  console.log(stringify(gendiff(parseFile1, parseFile2)));
-};
-
-export default diff;
+export default buildDiffTree;
