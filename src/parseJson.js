0573a1410e32754290123c33194a527fc1b80164
kaolin223 <m-kuptsov@inbox.ru>
2024-03-15 13:17:44 +0300

    refactor: Step4 fix grammar

--- src/parseJson.js
+++ src/parseJson.js
@@ -4,12 +4,12 @@ import process from 'node:process';
 
 const getData = (str) => {
   let data = {};
-  if (str.startWith('/')){
+  if (str.startsWith('/')){
     data = path.resolve(str);
   } else {
     const directoryName = process.cwd(str);
     data = path.resolve(directoryName, str);
-  };
+  }
   return JSON.parse(fs.readFileSync(data));
 }
 
