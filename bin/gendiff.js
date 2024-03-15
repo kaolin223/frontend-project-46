73262e6e1376dd40550731dcbd332db8499cac39
kaolin223 <m-kuptsov@inbox.ru>
2024-03-15 13:12:58 +0300

    refactor: Step4 update gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,8 +9,9 @@ program
   .name('gendiff')
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .argument('<filepath1>', 'path to file')
-  .argument('<filepath2>', 'path to file')
-  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
+
+  program.parse();
   
